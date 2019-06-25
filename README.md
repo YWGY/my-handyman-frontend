234cf61015049d274e072274a5b06ab168665f71
Weiguiyun Yang <n10022481@qut.edu.au>
2019-06-25 22:13:45 +1000

    update file

--- README.md
+++
@@ -1,2 +0,0 @@
-# my-handyman-frontend
-front-end
---
+++ demo-app/src/components/App/Routes.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Switch, Route} from 'react-router-dom';
+
+import businessCard from '../Business/BusinessCard';
+import businessDetailDisplay from '../Business/BusinessDetailDisplay';
+import businessDetailView from '../Business/BusinessDetailView';
+import businessEditView from '../Business/BusinessEditView';
+import businessView from '../Business/BusinessView';
+
+// export default () => (
+//     <Switch>
+
+//         <Route exact path='/' component={}
+
+//     </Switch>
+// )
